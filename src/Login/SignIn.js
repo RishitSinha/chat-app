ccc903af727f8c4b0dbac32bd900eb5dcaba45ab
Paras Chaudhary <parasb10chd@iitkgp.ac.in>
2017-07-08 15:22:42 +0530

    trial login bug fix

--- src/Login/SignIn.js
+++ src/Login/SignIn.js
@@ -26,8 +26,12 @@ export default class SignIn extends Component {
 
             data: {function_name: 'logins', username: this.refs.username.value, password: this.refs.password.value},
             dataType: 'json',
-            success: function (data) {
-                this.props.setlogin(this.refs.username.value);
+            success: function (data) {   if (data['code'] == 1) {
+                    alert("no user found");
+                }
+                else {
+                    this.props.setlogin(this.refs.username.value);
+                }
             }.bind(this),
             error: function (data) {
             }.bind(this),
